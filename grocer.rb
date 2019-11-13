2fb69a6586d19d45751fbae443fd8c67c02d1361
Alejandro Linan <jandrolinan@gmail.com>
2019-11-13 18:07:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -87,7 +87,7 @@ def apply_clearance(cart)
   while i < cart.length do 
     if cart[i][:clearance]
       cart[i][:price] = cart[i][:price] * .80
-      cart[i][:price].float 
+       
     else 
       
     end 
